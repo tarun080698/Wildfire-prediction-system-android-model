dce428050f782914a13311d1c9fe6df2cbbcb61d
tarun080698 <tarun3dadlani@gmail.com>
2020-03-04 23:58:45 +0530

    null

--- app/src/main/java/com/example/forestfire/AlertsActivity.java
+++ app/src/main/java/com/example/forestfire/AlertsActivity.java
@@ -1,27 +1,36 @@
 package com.example.forestfire;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.TaskStackBuilder;
-import android.content.Context;
-import android.content.Intent;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static com.example.forestfire.MyHelper.COLUMN_ALT;
+import static com.example.forestfire.MyHelper.COLUMN_ATM_P;
+import static com.example.forestfire.MyHelper.COLUMN_D_T;
+import static com.example.forestfire.MyHelper.COLUMN_HUMIDITY;
+import static com.example.forestfire.MyHelper.COLUMN_INTENSITY;
+import static com.example.forestfire.MyHelper.COLUMN_INTENSITY_RES;
+import static com.example.forestfire.MyHelper.COLUMN_SM;
+import static com.example.forestfire.MyHelper.COLUMN_TEMP;
 
 public class AlertsActivity extends AppCompatActivity {
 
+
     MyHelper helper;
+    public SQLiteDatabase databaseWrite;
     RecyclerView recyclerView;
+    MyAlertAdapter alertAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,48 +38,58 @@ public class AlertsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_alerts);
 
         helper = new MyHelper(this);
-        final SQLiteDatabase database = helper.getReadableDatabase();
+        databaseWrite = helper.getWritableDatabase();
+        SQLiteDatabase databaseRead = helper.getReadableDatabase();
 
-        Cursor cursor = database.rawQuery("SELECT " + MyHelper.COLUMN_D_T + ", " + MyHelper.COLUMN_TEMP + ", " +
-                MyHelper.COLUMN_HUMIDITY + ", " + MyHelper.COLUMN_SM + ", " + MyHelper.COLUMN_ATM_P + ", " +
-                MyHelper.COLUMN_ALT + ", " + MyHelper.COLUMN_INTENSITY_RES + " FROM past_alerts", new String[]{});
-
-        if (cursor != null) {
-            cursor.moveToFirst();
-        }
-        do {
-            assert cursor != null;
-            String date = cursor.getString(1);
-            Double temp = cursor.getDouble(2);
-            Double humidity = cursor.getDouble(3);
-            Double moist = cursor.getDouble(4);
-            Double pressure = cursor.getDouble(5);
-            Double alt = cursor.getDouble(6);
-            Double result = cursor.getDouble(7);
-
-        } while (cursor.moveToNext());
+        recyclerView = findViewById(R.id.logs_recycler);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setReverseLayout(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        alertAdapter = new MyAlertAdapter(this, getAllItems());
+        recyclerView.setAdapter(alertAdapter);
+        Button b = findViewById(R.id.btn);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insertData(33, 22, 99, 22, 22, "low", 22, databaseWrite);
+            }
+        });
 
 
-//        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                helper.insertData(33, 22, 22, 22, 22, "low", 22, database);
-//            }
-//        });
-
-//        recyclerView = findViewById(R.id.logs_recycler);
-//        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
-//        layoutManager.setStackFromEnd(true);
-//        layoutManager.setReverseLayout(true);
-//        recyclerView.setHasFixedSize(true);
-//        recyclerView.setLayoutManager(layoutManager);
-//        HomeFrag homeFrag = new HomeFrag();
-//        int i = homeFrag.intensity;
-
-//        if(i<30){
-//            inboxStyleNotification();
-//        }
     }
+
+    public void insertData(double temp, double humidity, double soil_moisture, double atm_p, double altitude, String iResult, double intensity, SQLiteDatabase db) {
+
+        ContentValues values = new ContentValues();
+        SimpleDateFormat sdf = new SimpleDateFormat("*yyyy-MM-dd hh:mm a", Locale.getDefault());
+        String currentDateAndTime = sdf.format(new Date());
+        values.put(COLUMN_D_T, currentDateAndTime);
+        values.put(COLUMN_TEMP, temp);
+        values.put(COLUMN_HUMIDITY, humidity);
+        values.put(COLUMN_SM, soil_moisture);
+        values.put(COLUMN_ATM_P, atm_p);
+        values.put(COLUMN_ALT, altitude);
+        values.put(COLUMN_INTENSITY_RES, iResult);
+        values.put(COLUMN_INTENSITY, intensity);
+
+        db.insert("past_alerts", null, values);
+
+        alertAdapter.swapCursor(getAllItems());
+    }
+
+    private Cursor getAllItems() {
+        return databaseWrite.query(
+                MyHelper.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                COLUMN_INTENSITY + " DESC");
+    }
+}
+
 //    public void simpleNotification(View view) {
 //        /**Id for Notification**/
 //        final String channelId = "Channel 1";
@@ -144,57 +163,97 @@ public class AlertsActivity extends AppCompatActivity {
 //            mNotificationManager.notify(1, builder.build());
 //        }
 //    }
-    public void inboxStyleNotification(View view) {
-        final String channelId = "Channel 1";
 
-        NotificationCompat.Builder mBuilder;
-        mBuilder = new NotificationCompat.Builder(this, channelId).setSmallIcon(R.drawable.app_logo_img)
-                .setContentTitle("ALERT!!!")
-                .setContentText("Seems like it's an alert for some serious fire alerts.")
-                .setSubText("Please check once!")
-                .setAutoCancel(true);
+//        Cursor cursor = databaseRead.rawQuery("SELECT " + MyHelper.COLUMN_D_T + ", " + MyHelper.COLUMN_TEMP + ", " +
+//                MyHelper.COLUMN_HUMIDITY + ", " + MyHelper.COLUMN_SM + ", " + MyHelper.COLUMN_ATM_P + ", " +
+//                MyHelper.COLUMN_ALT + ", " + MyHelper.COLUMN_INTENSITY_RES + " FROM past_alerts", new String[]{});
+//
+//        if (cursor != null) {
+//            cursor.moveToFirst();
+//        }
+//        do {
+//            assert cursor != null;
+//            int id = cursor.getInt(0);
+//            String date = cursor.getString(1);
+//            Double temp = cursor.getDouble(2);
+//            Double humidity = cursor.getDouble(3);
+//            Double moist = cursor.getDouble(4);
+//            Double pressure = cursor.getDouble(5);
+//            Double alt = cursor.getDouble(6);
+//            String result = cursor.getString(7);
+//
+//        } while (cursor.moveToNext());
 
-        /**Set a Notification Style to Inbox**/
-        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
-        /**Set a Title When Notification Expanded**/
-        inboxStyle.setBigContentTitle("It's an alert.");
 
-        /**Add your All messages here or use Loop to generate messages**/
+//        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                helper.insertData(33, 22, 22, 22, 22, "low", 22, database);
+//            }
+//        });
 
-        inboxStyle.addLine("Message 1");
-        inboxStyle.addLine("Message 2");
-        inboxStyle.addLine("Message 3");
-        inboxStyle.addLine("Message 4");
-        inboxStyle.addLine("Message 5");
+//        recyclerView = findViewById(R.id.logs_recycler);
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+//        layoutManager.setStackFromEnd(true);
+//        layoutManager.setReverseLayout(true);
+//        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(layoutManager);
+//        HomeFrag homeFrag = new HomeFrag();
+//        int i = homeFrag.intensity;
+//        if(i<30){
+//            inboxStyleNotification();
+//        }
 
-        /**Set the NotificationCompat object to builder**/
-
-        mBuilder.setStyle(inboxStyle);
-        Intent intent = getIntent();
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        stackBuilder.addNextIntent(intent);
-        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        mBuilder.setContentIntent(pIntent);
-
-        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
-            String channelId2 = "2";
-            String channelName2 = "channel2";
-            NotificationChannel channel = new NotificationChannel(channelId2, channelName2, NotificationManager.IMPORTANCE_DEFAULT);
-            channel.enableLights(true);
-            channel.setLightColor(Color.RED);
-            channel.setShowBadge(true);
-            channel.enableVibration(true);
-            mBuilder.setChannelId(channelId2);
-            if (mNotificationManager != null) {
-                mNotificationManager.createNotificationChannel(channel);
-            }
-        } else {
-            mBuilder.setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE);
-        }
-        if (mNotificationManager != null) {
-            mNotificationManager.notify(1, mBuilder.build());
-        }
-    }
-}
+//    public void inboxStyleNotification(View view) {
+//        final String channelId = "Channel 1";
+//
+//        NotificationCompat.Builder mBuilder;
+//        mBuilder = new NotificationCompat.Builder(this, channelId).setSmallIcon(R.drawable.app_logo_img)
+//                .setContentTitle("ALERT!!!")
+//                .setContentText("Seems like it's an alert for some serious fire alerts.")
+//                .setSubText("Please check once!")
+//                .setAutoCancel(true);
+//
+//        /**Set a Notification Style to Inbox**/
+//        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+//        /**Set a Title When Notification Expanded**/
+//        inboxStyle.setBigContentTitle("It's an alert.");
+//
+//        /**Add your All messages here or use Loop to generate messages**/
+//
+//        inboxStyle.addLine("Message 1");
+//        inboxStyle.addLine("Message 2");
+//        inboxStyle.addLine("Message 3");
+//        inboxStyle.addLine("Message 4");
+//        inboxStyle.addLine("Message 5");
+//
+//        /**Set the NotificationCompat object to builder**/
+//
+//        mBuilder.setStyle(inboxStyle);
+//        Intent intent = getIntent();
+//        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+//        stackBuilder.addNextIntent(intent);
+//        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//        mBuilder.setContentIntent(pIntent);
+//
+//        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//
+//            String channelId2 = "2";
+//            String channelName2 = "channel2";
+//            NotificationChannel channel = new NotificationChannel(channelId2, channelName2, NotificationManager.IMPORTANCE_DEFAULT);
+//            channel.enableLights(true);
+//            channel.setLightColor(Color.RED);
+//            channel.setShowBadge(true);
+//            channel.enableVibration(true);
+//            mBuilder.setChannelId(channelId2);
+//            if (mNotificationManager != null) {
+//                mNotificationManager.createNotificationChannel(channel);
+//            }
+//        } else {
+//            mBuilder.setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE);
+//        }
+//        if (mNotificationManager != null) {
+//            mNotificationManager.notify(1, mBuilder.build());
+//        }
+//    }
--- app/src/main/java/com/example/forestfire/MyHelper.java
+++ app/src/main/java/com/example/forestfire/MyHelper.java
@@ -1,20 +1,16 @@
 package com.example.forestfire;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 public class MyHelper extends SQLiteOpenHelper {
+
     private static final String dbname = "alerts.db";
     private static final int version = 1;
 
     static final String TABLE_NAME = "past_alerts";
-    static final String COLUMN_ID = "id";
+    static final String COLUMN_ID = "_id";
     static final String COLUMN_D_T = "date_time";
     static final String COLUMN_TEMP = "temperature";
     static final String COLUMN_HUMIDITY = "humidity";
@@ -24,6 +20,7 @@ public class MyHelper extends SQLiteOpenHelper {
     static final String COLUMN_INTENSITY_RES = "Intensity_result";
     static final String COLUMN_INTENSITY = "Intensity";
 
+
     MyHelper(Context context) {
         super(context, dbname, null, version);
     }
@@ -32,7 +29,7 @@ public class MyHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         String sqlCreateTable = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" +
                 COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
-                COLUMN_D_T + " TEXT NOT NULL," +
+                COLUMN_D_T + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP," +
                 COLUMN_TEMP + " REAL NOT NULL," +
                 COLUMN_HUMIDITY + " REAL NOT NULL," +
                 COLUMN_SM + " REAL NOT NULL," +
@@ -42,26 +39,25 @@ public class MyHelper extends SQLiteOpenHelper {
                 COLUMN_INTENSITY + " REAL NOT NULL)";
         db.execSQL(sqlCreateTable);
 
-        insertData(33, 22, 99, 22, 22, "low", 22, db);
     }
 
-    void insertData(double temp, double humidity, double soil_moisture, double atm_p, double altitude, String iResult, double intensity, SQLiteDatabase db) {
-
-        ContentValues values = new ContentValues();
-        SimpleDateFormat sdf = new SimpleDateFormat("*yyyy-MM-dd hh:mm a", Locale.getDefault());
-        String currentDateAndTime = sdf.format(new Date());
-        values.put(COLUMN_D_T, currentDateAndTime);
-        values.put(COLUMN_TEMP, temp);
-        values.put(COLUMN_HUMIDITY, humidity);
-        values.put(COLUMN_SM, soil_moisture);
-        values.put(COLUMN_ATM_P, atm_p);
-        values.put(COLUMN_ALT, altitude);
-        values.put(COLUMN_INTENSITY_RES, iResult);
-        values.put(COLUMN_INTENSITY, intensity);
-
-        db.insert("past_alerts", null, values);
-
-    }
+//    void insertData(double temp, double humidity, double soil_moisture, double atm_p, double altitude, String iResult, double intensity, SQLiteDatabase db) {
+//
+//        ContentValues values = new ContentValues();
+//        SimpleDateFormat sdf = new SimpleDateFormat("*yyyy-MM-dd hh:mm a", Locale.getDefault());
+//        String currentDateAndTime = sdf.format(new Date());
+//        values.put(COLUMN_D_T, currentDateAndTime);
+//        values.put(COLUMN_TEMP, temp);
+//        values.put(COLUMN_HUMIDITY, humidity);
+//        values.put(COLUMN_SM, soil_moisture);
+//        values.put(COLUMN_ATM_P, atm_p);
+//        values.put(COLUMN_ALT, altitude);
+//        values.put(COLUMN_INTENSITY_RES, iResult);
+//        values.put(COLUMN_INTENSITY, intensity);
+//
+//        db.insert("past_alerts", null, values);
+//
+//    }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
